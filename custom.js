d5bcee46c807b0d7bd9c691d12e965e0a0b8032a
rairoshan88 <rairoshan88@gmail.com>
2026-01-03 14:34:24 +0530

    chatbot removed

--- custom.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var pp = document.createElement('script'),
-        ppr = document.getElementsByTagName('script')[0];
-    stid = 'NFc1MW0zYlRzRXJqd3ZGZ2hwVCtpZz09';
-    pp.type = 'text/javascript';
-    pp.async = true;
-    pp.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 's01.live2support.com/dashboardv2/chatwindow/';
-    ppr.parentNode.insertBefore(pp, ppr);
-})();
